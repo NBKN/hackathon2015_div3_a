014fb19b909063ef447fe53178b93fec99a3f7a4
NBKN <knt.watanab@gmail.com>
2015-08-28 19:04:40 +0900

    added light box

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,3 @@
 //= require jquery
 //= require jquery_ujs
-
-(function($){
-
-  $(document).ready(function(){
-    var elem = document.getElementById("hello");
-
-    elem.style.backgroundColor="#9F9";
-  });
-
-})(jQuery);
+//= require lightbox
